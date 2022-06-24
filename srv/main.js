50292c4c7b2677156ef52b8f53299e6b5f360173
DJ Adams <dj.adams@sap.com>
2022-06-24 08:30:49 +0000

    add live stream content

--- srv/main.js
+++ srv/main.js
@@ -2,7 +2,28 @@ const cds = require('@sap/cds')
 
 class NorthwindModel extends cds.ApplicationService { init(){
 
-  console.log('In class NorthwindModel')
+  const { Products, Suppliers } = this.entities ('northwind')
+
+  // Unbound action
+  this.on ('submitOrder', async req => {
+
+    const { product, quantity } = req.data
+    const stockInfo = await SELECT `UnitsInStock` .from (Products, product)
+    const newStock = stockInfo.UnitsInStock - quantity
+    await UPDATE (Products, product) .with ({ UnitsInStock: newStock })
+    return newStock
+  })
+
+  // Bound action
+  this.on ('discontinue', 'Products', async ({params:[{ProductID}]}) => {
+    await UPDATE (Products, ProductID) .with ({
+      UnitsInStock: 0,
+      UnitsOnOrder: 0,
+      Discontinued: true
+    })
+    const product = await SELECT .from (Products, ProductID)
+    return product
+  })
 
   // Unbound function
   this.on('randomProduct', async req => {
@@ -11,6 +32,13 @@ class NorthwindModel extends cds.ApplicationService { init(){
     const product = await SELECT .from (Products, randomID)
     return product
   })
+
+  // Bound function
+  this.on ('addressLine', 'Suppliers', async ({params:[{SupplierID}]}) => {
+    const supplier = await SELECT .from (Suppliers, SupplierID)
+    return [supplier.Address, supplier.City, supplier.PostalCode, supplier.Country].join(', ')
+  })
+
   return super.init()
 }}
 
