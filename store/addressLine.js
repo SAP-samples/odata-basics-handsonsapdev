82afe833e662e3573b905c3df3f105f18bbb6d87
DJ Adams <dj.adams@sap.com>
2022-06-24 08:36:47 +0000

    remove temp store

--- store/addressLine.js
+++
@@ -1,5 +0,0 @@
-  // Bound function
-  this.on ('addressLine', 'Suppliers', async ({params:[{SupplierID}]}) => {
-    const supplier = await SELECT .from (Suppliers, SupplierID)
-    return [supplier.Address, supplier.City, supplier.PostalCode, supplier.Country].join(', ')
-  })
--- store/randomProduct.js
+++
@@ -1,7 +0,0 @@
-  // Unbound function
-  this.on('randomProduct', async req => {
-    const result = await SELECT .one.from (Products, ['max(ProductID) as max'])
-    const randomID = Math.floor(Math.random() * result.max) + 1
-    const product = await SELECT .from (Products, randomID)
-    return product
-  })
